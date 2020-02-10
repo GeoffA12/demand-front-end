6b30ac327390baa22e072a6a341d8eae37ba4e1d
garroyo <garroyo@stedwards.edu>
2020-02-10 00:46:30 +0000

    Changes made to accomodate for our server environment

--- login/login.py
+++ login/login.py
@@ -7,61 +7,51 @@ import mysql.connector as mariadb
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-   
     def do_POST(self):
         path = self.path
-        print("post")
-        if path == "/":  
-            mariadb_connection = mariadb.connect(user='root', password='ShinyNatu34', database='team22demand')
-            cursor = mariadb_connection.cursor()
-            rows = cursor.fetchall()
-            username_list = [x[0] for x in rows]
-     
-            length = int(self.headers['content-length'])            
+        print(path)
+        if path == "/loginHandler":
+            length = int(self.headers['content-length'])
             body = self.rfile.read(length)
-            
             # How to convert the body from a string to a dictionary
             # use 'loads' to convert from byte/string to a dictionary!
             dictionary = json.loads(body)
-            # To access a specific key from the dictionary:         
-            print(dictionary)    
+            # To access a specific key from the dictionary:
+            print(dictionary)
             username = dictionary['username']
-            password = dictionary['password']    
-            self.end_headers()
+            password = dictionary['password']
 
-        
             mariadb_connection = mariadb.connect(user='root', password='ShinyNatu34', database='team22demand')
             cursor = mariadb_connection.cursor()
-            testUser = "geoff"
-            cursor.execute("SELECT username FROM customers")
-
+            cursor.execute("SELECT username, password FROM customers")
             rows = cursor.fetchall()
             username_list = [x[0] for x in rows]
-            # check all usernames in the table to make sure we're keeping our usernames unique
+            password_list = [x[1] for x in rows]
+            # check all usernames and passwords in the table to make sure we're keeping our usernames unique
             userAlreadyExists = False
-            for x in username_list:
-	            print(x)
-	        if (testUser == username):
-		        userAlreadyExists = True
-		            break
+            for (x, y) in zip(username_list, password_list):
+                print(x)
+                print(y)
+                if (x == username and y == password):
+                    userAlreadyExists = True
+                    break
             response = None
             # We'll send a 401 code back to the client if the user hasn't registered in our database
             if (userAlreadyExists):
-	            response = 200
+                response = 200
             else:
-	            response = 401 
+                response = 401
             print(response)
-
             self.send_response(response)
-                      
-            responseDict = {}            
-            responseDict['success'] = True            
-            responseDict['otherParams'] = "here"            
+            self.end_headers()
+            responseDict = {}
+            responseDict['success'] = True
+            responseDict['otherParams'] = "here"
             res = json.dumps(responseDict)
-            bytesStr = res.encode('utf-8')            
+            bytesStr = res.encode('utf-8')
             self.wfile.write(bytesStr)
-        else:     
-            self.send_response(404)            
+        else:
+            self.send_response(404)
             self.end_headers()
 
     def do_GET(self):
@@ -78,15 +68,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 def main():
     # Define the port your server will run on:
     # Using 4001 as an example! Yours may run on another port!
-    # Consult with Devops Coordinator to find out which port 
-    # your server should be running on!    
+    # Consult with Devops Coordinator to find out which port
+    # your server should be running on!
     port = 4001
-    # Create an http server using the class and port you defined    
-    httpServer = http.server.HTTPServer (('', port),SimpleHTTPRequestHandler)    
+    # Create an http server using the class and port you defined
+    httpServer = http.server.HTTPServer (('', port),SimpleHTTPRequestHandler)
     print("Running on port", port)
     # this next call is blocking! So consult with Devops Coordinator for
     # instructions on how to run without blocking other commands frombeing
-    # executed in your terminal!    
+    # executed in your terminal!
     httpServer.serve_forever()
 
 if __name__ == "__main__":
--- register/register.py
+++ register/register.py
@@ -6,61 +6,58 @@ import mysql.connector as mariadb
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-     mariadb_connection = mariadb.connect(user='root', password='ShinyNatu34', database='team22demand')
-     cursor = mariadb_connection.cursor()
-     cursor.execute("SELECT username FROM customers")
-     rows = cursor.fetchall()
-     usernames = [x[0] for x in rows]
 
     def do_POST(self):
         path = self.path
-        print("post")
-        if path == "/": 
+        print(path)
+        print("/registerHandler")
+        if path == "/registerHandler":
             mariadb_connection = mariadb.connect(user='root', password='ShinyNatu34', database='team22demand')
             cursor = mariadb_connection.cursor()
             cursor.execute("SELECT username FROM customers")
             rows = cursor.fetchall()
-            usernames = [x[0] for x in rows]      
-            length = int(self.headers['content-length'])            
+            usernames = [x[0] for x in rows]
+            length = int(self.headers['content-length'])
             body = self.rfile.read(length)
-            
-            # How to convert the body from a string to a dictionary
-            # use 'loads' to convert from byte/string to a dictionary!
+
+            	# How to convert the body from a string to a dictionary
+            	# use 'loads' to convert from byte/string to a dictionary!
             dictionary = json.loads(body)
-            # To access a specific key from the dictionary:         
-            print(dictionary)    
+            	# To access a specific key from the dictionary:
+            print(dictionary)
             username = dictionary['username']
             password = dictionary['password']
             email = dictionary['email']
-            phone = dictionary['phone']    
-            self.end_headers()
+            phone = dictionary['phoneNumber']
 
             userAlreadyExists = False
             for x in usernames:
-	            if (newUser == username):
-		            userAlreadyExists = True
+                if (x == username):
+                    userAlreadyExists = True
 
             response = None
             if (userAlreadyExists):
-	            response = 401
+                response = 401
             else:
-	            response = 200
-	        newCursor = mariadb_connection.cursor()
-	        newCursor.execute("INSERT INTO customers (username, password, email, phone) VALUES (%s, %s, %s, %s)", (username, password, email, phone))
-	        mariadb_connection.commit()
+                response = 200
+                newCursor = mariadb_connection.cursor()
+                print(username)
+                print(password)
+                newCursor.execute("INSERT INTO customers (username, password, email, phone) VALUES (%s, %s, %s, %s)", (username, password, email, phone))
+                mariadb_connection.commit()
             print(response)
-            return self.response(response)
-
-            
-                      
-            responseDict = {}            
-            responseDict['success'] = True            
-            responseDict['otherParams'] = "here"            
+            self.send_response(response)
+            self.end_headers()
+            responseDict = {}
+            responseDict['success'] = True
+            responseDict['otherParams'] = "here"
             res = json.dumps(responseDict)
-            bytesStr = res.encode('utf-8')            
+            bytesStr = res.encode('utf-8')
             self.wfile.write(bytesStr)
-        else:     
-            self.send_response(404)            
+        else:
+            print("I got to your handler but I didn't find the correct path")
+            #self.wfile.write(p.encode('utf-8'))
+            self.send_response(405)
             self.end_headers()
 
     def do_GET(self):
@@ -77,15 +74,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 def main():
     # Define the port your server will run on:
     # Using 4001 as an example! Yours may run on another port!
-    # Consult with Devops Coordinator to find out which port 
-    # your server should be running on!    
+    # Consult with Devops Coordinator to find out which port
+    # your server should be running on!
     port = 4002
-    # Create an http server using the class and port you defined    
-    httpServer = http.server.HTTPServer (('', port),SimpleHTTPRequestHandler)    
+    # Create an http server using the class and port you defined
+    httpServer = http.server.HTTPServer (('', port),SimpleHTTPRequestHandler)
     print("Running on port", port)
     # this next call is blocking! So consult with Devops Coordinator for
     # instructions on how to run without blocking other commands frombeing
-    # executed in your terminal!    
+    # executed in your terminal!
     httpServer.serve_forever()
 
 if __name__ == "__main__":
